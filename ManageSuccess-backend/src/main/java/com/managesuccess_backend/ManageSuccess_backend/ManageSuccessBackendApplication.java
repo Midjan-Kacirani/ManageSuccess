c9ca52a60dc94ba9adae4153114ec26fca22a772
Midjan <midjankacirani@gmail.com>
2024-09-11 20:17:51 +0200

    Kalimi i files nga repo lokal ne remote repo

---
+++ ManageSuccess-backend/src/main/java/com/managesuccess_backend/ManageSuccess_backend/ManageSuccessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.managesuccess_backend.ManageSuccess_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageSuccessBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageSuccessBackendApplication.class, args);
+	}
+
+}
---
+++ ManageSuccess-backend/src/test/java/com/managesuccess_backend/ManageSuccess_backend/ManageSuccessBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managesuccess_backend.ManageSuccess_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageSuccessBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
